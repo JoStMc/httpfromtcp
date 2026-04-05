f23e345c90af71bd5d3cfc0c3ebff64e3caf71b3
JoStMc <joshmcdonnell@hotmail.co.uk>
2026-04-05 11:15:44 +0100

    channel refactor

--- main.go
+++ main.go
@@ -14,28 +14,42 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	var currentLine string
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if errors.Is(err, io.EOF){
+	linesCh := getLinesChannel(f)
+	for line := range linesCh {
+		fmt.Printf("read: %s\n", line)
+	} 
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesCh := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(linesCh)
+		var currentLine string
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if errors.Is(err, io.EOF){
+					if currentLine != "" {
+						linesCh <- currentLine
+					} 
+					break
+				} 
+				fmt.Printf("error: %s\n", err.Error())
 				break
-			} 
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
+			}
 
-		str := string(b[:n])
-		lines := strings.Split(str, "\n")
-		currentLine = currentLine + lines[0]
-		if len(lines) > 1 {
+			str := string(b[:n])
+			lines := strings.Split(str, "\n")
+			currentLine = currentLine + lines[0]
 			for i := 0; i < len(lines) - 1; i++ {
-				fmt.Printf("read: %s\n", currentLine)
+				linesCh <- currentLine
 				currentLine = lines[i+1]
 			} 
 		} 
-	} 
+	}()
+	return linesCh
 }
